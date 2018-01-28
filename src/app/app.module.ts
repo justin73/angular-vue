c2091045ea6e982b70a3a629e8cd8f88b293c353
Meng Jia <jiameng_73@hotmail.com>
2018-01-28 00:32:40 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
+// import { VueElement } from 'vue-hi-there';
 // import 'my-test-vue-custom-element';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // VueElement
   ],
   imports: [
     BrowserModule
